7cf69a756032656da9b987640c2b63668420018b
bayer <bbayerdev@gmail.com>
2025-01-13 11:40:42 -0300

    add button google in login/signUp (front only)

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -13,6 +13,7 @@ import { BorderBeam } from "@/components/ui/border-beam"
 import { useTheme } from "next-themes";
 import Particles from "@/components/ui/particles"
 
+
 const schema = z.object({
     email: z.string().email('Please enter a valid email address').nonempty('Please enter a valid email address'),
     password: z.string().nonempty('Please enter your password')
@@ -43,6 +44,10 @@ export default function Home() {
     const onSubmit = (data: FormData) => {
         console.log(data);
     };
+
+    useEffect(() => {
+        localStorage.clear()
+    }, [])
 
     return (
         <main className="flex justify-center items-center h-screen font-[family-name:var(--font-geist-sans)] overflow-hidden">
@@ -90,6 +95,17 @@ export default function Home() {
 
                         <div>
                             <Button type="submit" className="w-full rounded-[6px] mt-2">Login</Button>
+                        </div>
+                        <div>
+                            <Button type="submit" variant={'secondary'} className="w-full rounded-[6px] mt-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <path
+                                        d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                                        fill="currentColor"
+                                    />
+                                </svg>
+                                Login with Google
+                            </Button>
                         </div>
                     </form>
                 </CardContent>
--- app/signUp/page.tsx
+++ app/signUp/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Eye, EyeClosed, Info } from "lucide-react"
+import { Eye, EyeClosed, Info, UserPen } from "lucide-react"
 import Link from "next/link"
 import { useEffect, useState } from "react"
 import { useForm } from "react-hook-form";
@@ -180,8 +180,18 @@ export default function Home() {
                             </div>
                         </div>
 
-                        <div>
-                            <Button type="submit" className="w-full rounded-[6px] mt-2">Sign up</Button>
+                        <div className="flex gap-2">
+                            <Button type="submit" className="w-full rounded-[6px] mt-2">    <UserPen /> Sign up</Button>
+
+                            <Button type="submit" variant={'secondary'} className="w-full rounded-[6px] mt-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                    <path
+                                        d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                                        fill="currentColor"
+                                    />
+                                </svg>
+                                SignUp with Google
+                            </Button>
                         </div>
                     </form>
                 </CardContent>
