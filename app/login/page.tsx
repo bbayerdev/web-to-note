a88d4d8e68c419fc538fc0a7cea37ddd101edf8c
Cristiane Regina Masçon Marcon <bbayerdev@gmail.com>
2025-01-18 16:02:16 -0300

    page login 2.3

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -17,6 +17,7 @@ import { useRouter } from 'next/navigation';
 import { Toaster } from "@/components/ui/toaster"
 import { cn } from "@/lib/utils"
 import AnimatedGridPattern from "@/components/ui/animated-grid-pattern"
+import { Separator } from "@/components/ui/separator"
 
 const schema = z.object({
     email: z.string().email('Please enter a valid email address').nonempty('Please enter a valid email address'),
@@ -147,12 +148,17 @@ export default function Home() {
                                 <Link href='#' className="text-xs mt-1 text-muted-foreground hover:underline hover:text-white">Forgot your password?</Link>
                             </div>
                         </div>
-
+                         
                         <div>
                             <Button type="submit" className="w-full rounded-[6px] mt-2">Login</Button>
                         </div>
+                        <div className="justify-center items-center gap-2 flex w-full">
+                            <Separator orientation="horizontal" className="w-20" />
+                            <p className="text-xs text-muted-foreground">Or log in with</p>
+                            <Separator orientation="horizontal" className="w-20" />
+                        </div>
                         <div>
-                            <Button type="submit" variant={'secondary'} className="w-full rounded-[6px] mt-2">
+                            <Button type="submit" variant={'secondary'} className="w-full rounded-[6px]">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                     <path
                                         d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
