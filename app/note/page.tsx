7ceb55bba771d62ec50e2efa6b486c634d439d33
bayer <bbayerdev@gmail.com>
2025-02-13 22:15:32 -0300

    home note page layout 1.0

--- app/note/page.tsx
+++ app/note/page.tsx
@@ -1,15 +1,26 @@
 'use client'
 
 import { TypingAnimation } from "@/components/magicui/typing-animation"
+import { Button } from "@/components/ui/button"
+import { FilePlus2 } from "lucide-react"
 
 export default function NotePage() {
 
   return (
-    <main className="flex flex-col h-screen items-center font-[family-name:var(--font-geist-sans)]">
-      <TypingAnimation duration={70} className="mt-8 text-3xl">Every great idea starts with a note. How about writing something?</TypingAnimation>
-      <div>
-        
+    <main className="flex flex-col h-screen font-[family-name:var(--font-geist-sans)]">
+      <div className="flex justify-center">
+        <TypingAnimation duration={70} className="mt-8 text-3xl">Every great idea starts with a note. How about writing something?</TypingAnimation>
       </div>
+
+      <section className="flex flex-row w-full h-full px-20 pt-10 pb-20 gap-20">
+        <Button variant={'secondary'} className="w-full h-1/2 rounded-2xl bg-neutral-900">
+      
+        </Button>
+        <div className="bg-neutral-900 w-full h-full rounded-2xl">
+
+        </div>
+      </section>
+
     </main>
   )
 }
