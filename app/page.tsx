b00fb48bcb65bf21efa81ff2d5d2dd4769c70dcd
bayer <bbayerdev@gmail.com>
2025-02-16 21:06:11 -0300

    to-note 1.0 finished

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,7 @@ import Link from "next/link";
 export default function page() {
   return (
     <main>
-      <h1>root page</h1>
-
-      <div className="flex flex-col underline ml-2">
-        <Link href={'/login'}> login</Link>
-        <Link href={'/signUp'}> signUp</Link>
-
-        <Link href={'/note'}>
-          <Button variant={'destructive'}>to note</Button>
-        </Link>
-      </div>
-
+      
     </main>
   )
 
